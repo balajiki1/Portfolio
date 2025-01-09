66f69164f40ea69bdb2d696962006759cacfe7e6
balajiki1 <balaji.ki@northeastern.edu>
2025-01-08 20:29:28 -0500

    Prepare for GitHub Pages deployment

--- next.config.cjs
+++ next.config.cjs
@@ -1,15 +1,15 @@
 const isGithubActions = process.env.GITHUB_ACTIONS || false;
 
-const repo = process.env.GITHUB_REPOSITORY?.split('/')[1] || 'Portfolio'; // Use the repo name
+const repo = process.env.GITHUB_REPOSITORY?.split('/')[1] || 'Portfolio';
 const assetPrefix = isGithubActions ? `/${repo}/` : '';
 const basePath = isGithubActions ? `/${repo}` : '';
 
 module.exports = {
-  output: 'export', // Enables static export for Next.js
+  output: 'export', // Enables static export
   assetPrefix: assetPrefix,
   basePath: basePath,
   images: {
-    unoptimized: true,
+    unoptimized: true, // Disable image optimization
   },
-  trailingSlash: true,
+  trailingSlash: true, // Ensures proper routing
 };
